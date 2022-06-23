7b5a4495e0c686c5461bc606d2ffe693c2d89cb8
andreas.marneris <andreas.marneris@kidsloop.live>
2022-06-23 23:28:46 +0100

    fix: removed redundant variables

--- variables.tf
+++ variables.tf
@@ -1,17 +1,3 @@
-variable "kubernetes_server_urls" {
-  type = list(object({
-    environment = string
-    url         = string
-  }))
-
-  description = "All the target Kubernetes cluster urls"
-
-  default = [{
-    environment = "apifactory-uk"
-    url         = "https://EFC4B481A0B23459D26FE96D8985F7E3.gr7.eu-west-2.eks.amazonaws.com"
-  }]
-}
-
 # This is set in TFE workspace and is being consumed here
 variable "tools_cluster_tfc_sa_token" {
   description = "Service account token for Kubernetes service account to connect to Tools cluster"
